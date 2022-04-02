848888e12e3946421471c5492d22d7927479ff6b
jwf <2545538936@qq.com>
2022-04-02 12:08:27 +0800

    项目初始化

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+//路由文件
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+const routes = [{
+    path: '/',
+    name: 'Home',
+    component: Home
+}]
+const router = new VueRouter({
+    routes
+})
+export default router
